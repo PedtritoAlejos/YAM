9be12f9577f1278b351fa79001c5b353404da408
PedtritoAlejos <pedroalejosb@gmail.com>
2021-09-20 17:05:45 -0300

    Initial Commit

---
+++ JAM/JAMApp.swift
@@ -0,0 +1,17 @@
+//
+//  JAMApp.swift
+//  JAM
+//
+//  Created by pealejosb on 20-09-21.
+//
+
+import SwiftUI
+
+@main
+struct JAMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
